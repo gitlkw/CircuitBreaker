b5100617a46fa641d1067a4108860ba8b4f5e789
liukangwei <liukangwei@daojia-inc.com>
2020-09-03 03:00:44 +0800

    1

--- src/main/java/com/cn/lkw/CircuitBreakerManager.java
+++ src/main/java/com/cn/lkw/CircuitBreakerManager.java
@@ -30,28 +30,28 @@ public class CircuitBreakerManager {
         CircuitBreakerRunner runner = circuitBreakerStrategy.getCircuitBreakerRunner();
         for (; ; ) {
             final Long oldOpenTimeCurr = circuitBreakerStrategy.getOpenTimeCurr();
-            if (circuitBreakerStrategy.isOpen(oldOpenTimeCurr)) {
-                if (circuitBreakerStrategy.isTimeOut(oldOpenTimeCurr)) {
+            if (circuitBreakerStrategy.isOpen(oldOpenTimeCurr)) { // 开关处于打开状态
+                if (circuitBreakerStrategy.isTimeOut(oldOpenTimeCurr)) { // 开关如果没超时，执行fallback()
                     try {
                         runner.fallback();
                     } catch (Exception e) {
                         System.out.println(name + ":fallback error" + e.getMessage());
                     }
                     break;
-                } else {
+                } else {// 开关超时，尝试关闭开关
                     circuitBreakerStrategy.close(oldOpenTimeCurr);
                 }
-            } else {
+            } else { // 未打开，执行run()
                 try {
                     runner.run();
                 } catch (Exception e) {
-                    circuitBreakerStrategy.addErrorMaxCurr();
+                    circuitBreakerStrategy.addErrorMaxCurr(); // 记录失败次数
                     System.out.println(name + ":run error" + e.getMessage());
                 } finally {
-                    circuitBreakerStrategy.addLimitMaxCurr();
+                    circuitBreakerStrategy.addLimitMaxCurr(); // 记录总次数
                 }
-                if (circuitBreakerStrategy.checkErrorLimit()) {
-                    circuitBreakerStrategy.open();
+                if (circuitBreakerStrategy.checkErrorLimit()) { // 如果失败次数触发阀值
+                    circuitBreakerStrategy.open(); // 打开开关
                 }
                 break;
             }
